d7d7bb1c7ae5678f8ad3da3d068fc6b76ee6b2d1
aryan.aman <aryanaman@MacBook-Pro.local>
2022-09-16 11:46:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OneKgate';
+  
+  slides = [
+    {img: "https://dummyimage.com/350x150/423b42/fff"},
+    {img: "https://dummyimage.com/350x150/2a2b7a/fff"},
+    {img: "https://dummyimage.com/350x150/1a2b7a/fff"},
+    {img: "https://dummyimage.com/350x150/7a2b7a/fff"},
+    {img: "https://dummyimage.com/350x150/9a2b7a/fff"},
+    {img: "https://dummyimage.com/350x150/5a2b7a/fff"},
+    {img: "https://dummyimage.com/350x150/4a2b7a/fff"}
+  ];
+  slideConfig = {"slidesToShow": 4, "slidesToScroll": 4};
+  
+  slickInit(e:any) {
+    console.log('slick initialized');
+  }
+    
+  breakpoint(e:any) {
+    console.log('breakpoint');
+  }
+    
+  afterChange(e:any) {
+    console.log('afterChange');
+  }
+    
+  beforeChange(e:any) {
+    console.log('beforeChange');
+  }
 }
